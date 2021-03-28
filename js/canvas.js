a97a9d4432f23b0233b2c1702c89d18431a57213
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2021-03-28 16:21:39 +0200

    Visualize generated numbers into column chart

--- js/canvas.js
+++ js/canvas.js
@@ -1,23 +1,66 @@
+// Global variables:
 let sliderValue = 0;
 let randomArray = [];
 
+// Set default values on page load:
 function setValue(){
     document.getElementById("array-size").innerHTML = document.getElementById("slider").value;
     sliderValue = document.getElementById("slider").value;
+    generateArray();
 }
 
+// Set array-size value & Generate random numbers on slider value change:
 function sliderChange(value){
-    sliderValue = value;
+    setValue();
     document.getElementById("array-size").innerHTML = sliderValue;
     generateArray();
 }
 
 function generateArray(){
-    // Generate random numbers between 1 and 200:
+    // Clear visualizer:
+    let visualizer = document.getElementById("visualizer");
+    visualizer.innerHTML = "";
+
+    // Generate random numbers between 10 and 200:
     for (let i=0; i<sliderValue; i++){
-        let number = Math.floor((Math.random() * 200) + 1);
+        let number = Math.floor((Math.random() * 200) + 10);
         randomArray.push(number);
     }
-    console.log(randomArray);
+    // Display generated numbers:
+    visualizeNumbers();
+
+    // Clear numbers' array:
     randomArray = [];
+}
+
+function visualizeNumbers(){
+    // Modify (visualizer) element:
+    let visualizer = document.getElementById("visualizer");
+    visualizer.style.height = "device-height";
+    visualizer.style.display = "flex";
+    visualizer.style.flexDirection = "row";
+    visualizer.style.justifyContent = "center";
+
+    // Display generated numbers:
+    for (let i=0; i<sliderValue; i++){
+        // Create new element of type (div):
+        let element = document.createElement("div");
+
+        // Specify new element's attributes:
+        element.style.backgroundColor = "#555555";
+        element.style.width = "30px";
+        element.style.height = "100px"
+        element.style.margin = "5px";
+        element.style.padding = "5px";
+        element.style.textAlign = "center";
+        if (sliderValue < 20){
+            element.innerHTML = randomArray[i];
+        }
+        else{
+            element.innerHTML = "";
+        }
+
+        // Add new element into (visualizer) element:
+        visualizer.appendChild(element);
+    }
 }
